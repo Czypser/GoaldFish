3477b1a68bafde48c3e9634a283810e865a80d81
Czysper <czypser@gmail.com>
2016-04-27 15:34:28 -0700

    Testing one more time

--- Projects/Personal/training/resources/views/pages/about.blade.php
+++ Projects/Personal/training/resources/views/pages/about.blade.php
@@ -1,5 +1,5 @@
 @extends('layout.default')
 @section('content')
-	this is my about page, now I am changing something.
-	
+	this is my about page, now I am changing something. Testing one more thing.
+
 @stop
