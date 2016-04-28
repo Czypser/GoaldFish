6bcc2b799144d89a8eb3588ad94f77cb5f3ba837
Czysper <czypser@gmail.com>
2016-04-27 23:36:42 -0700

    adjusting the sidebar

--- Projects/Personal/training/resources/views/layout/BootstrapSidebar.blade.php
+++ Projects/Personal/training/resources/views/layout/BootstrapSidebar.blade.php
@@ -7,16 +7,8 @@
             <li><a href="#">Export</a></li>
           </ul>
           <ul class="nav nav-sidebar">
+            {{ Auth::user()->password }}
             <li><a href="">Nav item</a></li>
-            <li><a href="">Nav item again</a></li>
-            <li><a href="">One more nav</a></li>
-            <li><a href="">Another nav item</a></li>
-            <li><a href="">More navigation</a></li>
-          </ul>
-          <ul class="nav nav-sidebar">
-            <li><a href="">Nav item again</a></li>
-            <li><a href="">One more nav</a></li>
-            <li><a href="">Another nav item</a></li>
           </ul>
           <button type="button" class="btn btn-secondary btn-lg btn-block">+ Add Goal</button>
         </div>
